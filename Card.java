663994b95ba85540c044378c5a58b218a9fda4b0
Nessahehe <vsumski24@sandomenico.org>
2024-01-08 11:05:32 -0800

    Progress 5.2.2

--- Card.java
+++ Card.java
@@ -3,12 +3,64 @@ import greenfoot.*;
 /**
  * Game Board for Triples
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Vanessa Sumski
+ * @1/8/23
  */
+
 
 public class Card extends Actor
 {
+   public enum Shape {
+       SQUARE, CIRCLE, TRIANGLE
+   }
+   public enum Color {
+       RED, GREEN, BLUE
+   }
    
+   private Shape shape;
+   private Color color;
+   private boolean isSelected;
+   private GreenfootImage cardImage, selectedCardImage;
+   private int numberOfShapes, shading;
+   
+   public Card(Shape shape, Color color, int numberOfShapes, int shading, GreenfootImage cardImage, GreenfootImage selectedCardImage) 
+   {
+       this.shape = shape;
+       this.color = color;
+       this.numberOfShapes = numberOfShapes;
+       this.shading = shading;
+       this.cardImage = cardImage;
+       this.selectedCardImage = selectedCardImage;
+       
+       setImage(cardImage);
+    }
+    public Shape getShape() 
+    {
+        return shape;
+    }
+    public Color getColor()
+    {
+        return color;
+    }
+    public int getNumberOfShapes()
+    {
+        return numberOfShapes;
+    }
+    public int getShading()
+    {
+        return shading;
+    }
+    public GreenfootImage getCardImage()
+    {
+        return cardImage;
+    }
+    public GreenfootImage getSelectedCardImage()
+    {
+        return selectedCardImage;
+    }
+    public void setIsSelected(boolean isSelected)
+    {
+        this.isSelected = isSelected;
+    }
 }
 
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"Card.Color"},{"p":"<Unnamed>","l":"Deck"},{"p":"<Unnamed>","l":"GameBoard"},{"p":"<Unnamed>","l":"Card.Shape"}]
